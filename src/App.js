0515ea1bf26654ffaa006a6d68a7386960bb0fd8
kevalsolanki-decurtis <72786836+kevalsolanki-decurtis@users.noreply.github.com>
2020-10-14 17:42:12 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,96 @@
+import React, { useState } from "react";
+import Picker from "./component/Picker";
+import WheelPicker from "react-simple-wheel-picker";
+
+import "./styles.css";
+import styled from "styled-components";
+
+export default function App() {
+  const [valueGroups, setValueGroups] = useState({
+    title: "Mr.",
+    firstName: "Micheal",
+    secondName: "Jordan"
+  });
+
+  const handleChange = (name, value) => {
+    setValueGroups((valueGroups) => ({
+      valueGroups: {
+        ...valueGroups,
+        [name]: value
+      }
+    }));
+  };
+
+  const setKeyValue = (arr) => {
+    return arr.map((item) => {
+      const dataSet = {
+        id: item,
+        value: item
+      };
+      return dataSet;
+    });
+  };
+
+  const newOptionGroups = (optionGroups) => {
+    let groups = {};
+    for (const group in optionGroups) {
+      groups[group] = setKeyValue(optionGroups[group]);
+    }
+    return groups;
+  };
+  const optionGroups = {
+    title: ["Mr.", "Mrs.", "Ms.", "Dr."],
+    firstName: ["John", "Micheal", "Elizabeth"],
+    secondName: ["Lennon", "Jackson", "Jordan", "Legend", "Taylor"]
+  };
+
+  const opGroups = newOptionGroups(optionGroups);
+
+  let pickerColumn = [];
+  const handleOnChange = (target) => {
+    console.log(target);
+  };
+  for (const group in opGroups) {
+    const data = opGroups[group];
+
+    pickerColumn.push(
+      <StyledWheelPicker
+        data={data}
+        onChange={handleOnChange}
+        height={400}
+        width={100}
+        titleText="Enter value same as aria-label"
+        itemHeight={36}
+        selectedID={data[0].id}
+        color="#999999"
+        activeColor="#fff"
+        backgroundColor="black"
+        shadowColor="none"
+      />
+    );
+  }
+
+  console.log(opGroups);
+
+  return (
+    <div className="App">
+      <h1>Picker Test</h1>
+      {/* <Picker
+        onChange={handleChange}
+        valueGroups={valueGroups}
+        optionGroups={optionGroups}
+      /> */}
+      <PickerContainer>{pickerColumn}</PickerContainer>
+    </div>
+  );
+}
+
+const PickerContainer = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const StyledWheelPicker = styled(WheelPicker)`
+  box-shadow: none;
+  background-color: red;
+`;
---
+++ src/component/Picker.js
@@ -0,0 +1,54 @@
+import React from "react";
+import styled from "styled-components";
+import { v4 as uuid } from "uuid";
+
+const Picker = ({ onChange, optionGroups, valueGroups }) => {
+  console.log({ optionGroups });
+  console.log({ valueGroups });
+
+  const options = Object.keys(optionGroups);
+  const { title, firstName, secondName } = optionGroups;
+
+  const renderList = (arr) => {
+    return arr.map((item) => <PickerItem key={uuid()}>{item}</PickerItem>);
+  };
+
+  const renderColumns = options.map((option) => (
+    <PickerColumn key={uuid()}>{renderList(optionGroups[option])}</PickerColumn>
+  ));
+
+  return (
+    <PickerContainer>
+      <ColumnContainer>{renderColumns}</ColumnContainer>
+    </PickerContainer>
+  );
+};
+
+export default Picker;
+
+const PickerContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  background-color: pink;
+  align-items: flex-end;
+  height: 400px;
+`;
+
+const PickerColumn = styled.ul`
+  list-style: none;
+  padding: 0px;
+  margin: 0px;
+  background-color: green;
+  margin: 2px;
+  
+`;
+
+const PickerItem = styled.li`
+  padding: 10px;
+`;
+
+const ColumnContainer = styled.div`
+  background-color: yellow;
+  display: flex;
+  justify-content: center;
+`;
